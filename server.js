d2929dceb43177bfb6cfecc644013ac259a66627
PEERADACHLAORTUM <peeradach.l@gmail.com>
2017-05-31 14:16:41 +0700

    update util function

--- server.js
+++ server.js
@@ -2,10 +2,7 @@
 
 // Import hapi module into the project
 const Hapi = require('hapi');
-const Fs = require('fs');
-const Async = require('async');
-const Path = require('path');
-
+const util = require('./util')
 const routesDir = './routes';
 
 // Create a server with a host and port
@@ -13,32 +10,7 @@ const server = new Hapi.Server();
 server.connection({ port: 8000 });
 
 // Add the route
-Fs.readdir(routesDir, function (err, list) {
-
-        if (err) {
-            console.log(err);
-        }
-        Async.each(list, function (file, callback) {
-
-            // Skip files with unknown filename extension
-            if (!file.match(/\.(js)$/)) {
-                return callback();
-            }
-            // Change routes path to './'
-            var tmpdir = routesDir.split('/');
-            tmpdir[0] = '.';
-            var routesdir = tmpdir.join('/');
-
-            const modulePath = routesdir+ "/" +file;
-            const routeFile = require(modulePath);
-
-            // Register routes from path './routes'
-            if(typeof routeFile.routes != 'undefined') {
-                server.route(routeFile.routes);
-            }
-        });
-    });
-
+util.registerRoutes(server,routesDir);
 // Start the server
 server.start((err) => {
 
