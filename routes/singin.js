7d63449921ed13092fe7ea40b996b1a614e9640d
ardcsr <ard_csr@hotmail.com>
2017-05-31 17:25:43 +0700

    update singin.js

--- routes/singin.js
+++ routes/singin.js
@@ -4,13 +4,26 @@ exports.routes = [{
     method: 'GET',
     path: '/login/{user}/{pass}',
     handler: function (request, reply) {
-        var usernameinput = encodeURIComponent(request.params.user)
-        var passwordinput = encodeURIComponent(request.params.pass)
-    util.items.users.forEach(function(user){
-        if (user.){
+        var usernameinput = request.params.user
+        var passwordinput = request.params.pass
+        var i = 0
+        util.items.users.forEach(function(userInfo){
+            if (userInfo.user==usernameinput){
+                if(userInfo.passward==passwordinput){
+                    reply('Success')
+                    i=1
+                }
+                else{                 
+                     reply('password not macth')
+                     i=2
+                }
+                
+            }
             
-        }
-    })
+        })
+        if (i==0){
+            reply('user not found')
+        }        
 
             
     }
