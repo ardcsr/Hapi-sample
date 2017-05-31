9e8707b3686667326a221cd3b4cdf4f3ddaac7ab
Summer11671 <benz.11671@hotmail.com>
2017-05-31 14:36:31 +0700

    update server.js

--- server.js
+++ server.js
@@ -2,9 +2,7 @@
 
 // Import hapi module into the project
 const Hapi = require('hapi');
-const Fs = require('fs');
-const Async = require('async');
-const Path = require('path');
+const util = require('./util');
 
 const routesDir = './routes';
 
@@ -13,36 +11,11 @@ const server = new Hapi.Server();
 server.connection({ port: 8000 });
 
 // Add the route
-Fs.readdir(routesDir, function (err, list) {
+util.registerRoutes(server, routesDir);
 
-        if (err) {
-            console.log(err);
-        }
-        Async.each(list, function (file, callback) {
-
-            // Skip files with unknown filename extension
-            if (!file.match(/\.(js)$/)) {
-                return callback();
-            }
-            // Change routes path to './'
-            var tmpdir = routesDir.split('/');
-            tmpdir[0] = '.';
-            var routesdir = tmpdir.join('/');
-
-            const modulePath = routesdir+ "/" +file;
-            const routeFile = require(modulePath);
-
-            // Register routes from path './routes'
-            if(typeof routeFile.routes != 'undefined') {
-                server.route(routeFile.routes);
-            }
-        });
-    });
 server.on('response', function (request) {
     console.log(request.info.remoteAddress + ': ' + request.method.toUpperCase() + ' ' + request.url.path + ' --> ' + request.response.statusCode);
 });
-
-=======
 // Start the server
 server.start((err) => {
 
