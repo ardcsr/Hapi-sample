6a9f76afffba85aa5c59f251ea2c7be4b6d01218
nestwrsr <warisara_ntt@hotmail.com>
2017-06-01 10:20:02 +0700

    ของเค้า

--- routes/singin.js
+++ routes/singin.js
@@ -7,9 +7,7 @@ exports.routes = [{
         var usernameinput = encodeURIComponent(request.params.user)
         var passwordinput = encodeURIComponent(request.params.pass)
     util.items.users.forEach(function(user){
-        if (user.){
-            
-        }
+       
     })
 
             
